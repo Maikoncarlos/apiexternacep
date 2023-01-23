eb6a840bc0ed59a736ce213b0c7c041620d5328a
Maikoncarlos <maikonsaturno@gmail.com>
2023-01-23 20:56:01 -0300

    testando a anotação JsonProperty para identificar o atributo com o nome vindo da api externa e identificado em nossa api com nome diferente

--- src/main/java/com/github/maikoncarlos/apiexternacep/dto/response/EnderecoResponse.java
+++ src/main/java/com/github/maikoncarlos/apiexternacep/dto/response/EnderecoResponse.java
@@ -1,12 +1,14 @@
 package com.github.maikoncarlos.apiexternacep.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
 public class EnderecoResponse {
     
     private String cep;
-    private String logradouro;
+    @JsonProperty("logradouro")
+    private String rua;
     private String bairro;
     private String localidade;
     private String uf;
