7831d7a5e3d51d3ee4892da1cda75b05bb048e84
Maikoncarlos <maikonsaturno@gmail.com>
2023-01-23 20:57:10 -0300

    adaptando o tratamento de erro no consumo de uma api externa

--- src/main/java/com/github/maikoncarlos/apiexternacep/service/EnderecoService.java
+++ src/main/java/com/github/maikoncarlos/apiexternacep/service/EnderecoService.java
@@ -3,6 +3,7 @@ package com.github.maikoncarlos.apiexternacep.service;
 import com.github.maikoncarlos.apiexternacep.client.ComunicacaoApiExterna;
 import com.github.maikoncarlos.apiexternacep.dto.response.EnderecoResponse;
 import com.github.maikoncarlos.apiexternacep.exception.CepNullException;
+import feign.FeignException;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -17,15 +18,22 @@ public class EnderecoService {
     public EnderecoResponse findByCep(String cep) {
         log.info("COMUNICANDO COM API EXTERNA VIACEP");
         log.info("PESQUISA PELO CEP {} ", cep);
-        EnderecoResponse end = comunicacaoApiExterna.getEnderecoPorCep(cep);
-        boolean cepNull = (end.getLogradouro() == null);
-        if (cepNull) {
-            log.error("ERROR - CEP INVÁLIDO, RETORNO DO CEP COM VALORES NULOS {}", cepNull);
-            throw new CepNullException("cep inválido, retorno nulo");
-        } else {
-            log.info("RETORNO {} -> SUCESSO : ", end);
-            return end;
+
+        try {
+            EnderecoResponse end = comunicacaoApiExterna.getEnderecoPorCep(cep);
+            boolean cepNull = (end.getRua() == null);
+            if (cepNull) {
+                log.error("ERROR - CEP INVÁLIDO, O RETORNO DO CEP {} está {}", end.getCep(), end.getRua());
+                throw new CepNullException("Cep incorreto, endereço nulo!");
+            } else {
+                log.info("SUCESSO - RETORNO {}  : ", end);
+                return end;
+            }
+        }catch (FeignException ex){
+            log.error("Cep inválido  : " + ex.getMessage());
+            throw new CepNullException("Cep inválido  : " + ex.getMessage());
         }
+
     }
 }
 
